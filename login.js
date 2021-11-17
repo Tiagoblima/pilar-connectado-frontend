b80d3f80b86b9c701777878537fbe2e42a4f4a7a
Tiago de Lima <tiago.blima@ufrpe.br>
2021-11-17 07:45:17 -0300

    fixing loging error request

--- login.js
+++ login.js
@@ -1,39 +1,84 @@
 
 
-function fazPost(url,body)
+
+
+/*function  fazPost(url,body)
 {
     console.log("Body=", body)
     let request = new XMLHttpRequest()
     request.open("GET", url, true)
-   // request.setRequestHeader("Content-type","application/json")
+    //request.setRequestHeader("Content-type","application/json")
     request.setRequestHeader('Accept','application/json')
     request.withCredentials = 'same-origin';
     request.send(JSON.stringify(body))
 
-    request.onload = function resposta()
+    request.onload = async function resposta()
     {
-        console.log(this.responseText)
-        alert("usuario Conectado")
-        window.location.href('ListarUsuarios.html')
+        usu = JSON.parse(request.responseText)
     }
-
-    return request.responseText
+    return usu
 }
+
+
+const sendHttpRequest = (method, url, data) =>{
+    console.log(data)
+    return fetch(url, {
+        method:method,
+        credentials: 'same-origin',
+        redirect: 'follow',
+        agent: null,
+        headers: {
+            "Content-Type": "text/plain",
+            'Authorization': 'Basic ' + btoa(`${body["email"]}:body["senha"]`),
+        },
+        timeout: 5000
+    }).then(response => {
+        return response.json()
+    });
+}
+
+*/
+
+
+user = {"info":{}}
+
+// Example POST method implementation:
+async function credentialsRequest(url = '', data = {}) {
+    
+  
+    let username = data["email"];
+    let password = data["password"];
+
+    let headers = new Headers();
+    
+    headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
+
+    await fetch(url, {method:'GET',
+            headers: headers,
+            //credentials: 'user:passwd'
+        })
+    .then(response => response.json())
+    .then(json => user["info"] = json);
+  }
+  
+  
+
+  
 
 function Logar()
 {
     event.preventDefault()
-    let url = 'https://pilar-connectado.herokuapp.com/users/me'
+    let url = 'https://pilar-connectado.herokuapp.com/v1/users/me'
 
     let email = document.getElementById('login').value
     let senha = document.getElementById('senha').value
-    //console.log(email)
-    console.log(senha)
-
+    
     body = 
     {
         "email": email,
         "password": senha
     }
-    fazPost(url,body)
+    credentialsRequest(url, body)
+
+    console.log(user)
 }
