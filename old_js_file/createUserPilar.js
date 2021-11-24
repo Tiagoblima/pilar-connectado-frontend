f1937dca3426fac84f7971a86a3370ec1f01ef32
Tiago de Lima <tiago.blima@ufrpe.br>
2021-11-23 21:41:41 -0300

    organizando os arquivos

--- old_js_file/createUserPilar.js
+++ old_js_file/createUserPilar.js
@@ -36,3 +36,18 @@ partes.forEach(function (parte) {
 console.log(usuarioCad)
 
 main(usuarioCad)
+
+function fazPost(url,body)
+{
+    console.log("Body=", body)
+    let request = new XMLHttpRequest()
+    request.open("POST",url,true)
+    request.setRequestHeader("Content-type","application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload =  function resposta()
+    {
+        console.log(this.responseText)
+    }
+    return request.responseText
+}
