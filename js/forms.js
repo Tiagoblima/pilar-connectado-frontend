dc9c9fcfca2e00de0b14a7f87c6c6d33eeecd86d
Tiago de Lima <tiago.blima@ufrpe.br>
2021-11-23 22:00:01 -0300

    organizando os arquivos

--- js/forms.js
+++ js/forms.js
@@ -230,3 +230,31 @@ function cadastrarUsuarioPorto()
     console.log(body)
     sendPostMemberRequest(url,body)
 }
+
+
+getSkillsList = async function() {
+
+
+    let headers = new Headers();
+    
+    headers.append("Content-type","application/json");
+
+    await fetch(BASE_URL+'skill/', {
+        method: 'GET',
+        headers: headers,
+    }).then(response => response.json().then(data => {
+        console.log(data);
+        let selectDOM = document.getElementById("selectSkill")
+        data.forEach(element => {
+
+            let option = document.createElement("option");
+            option.value = element.id;
+            option.text = element.name;
+            selectDOM.appendChild(option);
+        }
+        )
+    })).catch(error => console.error('Error:', error));
+
+
+}
+getSkillsList();
