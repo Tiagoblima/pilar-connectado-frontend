33e0a4045e4b1196deedc73de3acff825217a60b
Tiago de Lima <tiago.blima@ufrpe.br>
2021-11-27 22:01:51 -0300

    rendering users basic info

--- js/profile.js
+++ js/profile.js
@@ -16,11 +16,62 @@ function showPilarMemberDetails()
 {
   
 
+
+
+
+
+  getByIdRequest("pilar_member/by/user/", localStorage.getItem("userId")).then(
+
+    (data) => {
+
+      console.log(data);
+
+      document.getElementById("instagram").innerHTML = data.instagram;
+      document.getElementById("introduction").innerHTML = data.introduction;
+
+      // <span class="fa fa-star" style="color: goldenrod;"></span>
+
+      const stars = data.evaluation;
+
+      let stars_html =  document.getElementById("stars");
+
+
+      for(let i = 0; i < stars; i++){
+
+
+        let star = document.createElement("span");
+        star.className = "fa fa-star";
+        star.style.color = "goldenrod";
+        stars_html.appendChild(star);
+        
+
+      }
+
+      showPosters(data.id);
+
+
+    }
+
+  );
+
 }
 
-function showUserDetails(user)
+function showUserDetails(user_id)
 {
-    
+    getByIdRequest("user/by/id/", user_id).then(
+
+      (data) => {
+
+        document.getElementById("name").innerHTML = data.name;
+        document.getElementById("email").innerHTML = data.email;
+        document.getElementById("cpf").innerHTML = data.cpf; 
+        document.getElementById("address").innerHTML = data.address;
+      
+        console.log(data);
+      }
+
+
+    )
 }
 
 function showPosters(pilarMemberId){
@@ -31,6 +82,10 @@ function showPosters(pilarMemberId){
 
 function main(){
 
+  localStorage.setItem("userId", 1)
+
+  showUserDetails(localStorage.getItem("userId"));
+  showPilarMemberDetails()
   console.log(localStorage.getItem("userId"));
 
 
