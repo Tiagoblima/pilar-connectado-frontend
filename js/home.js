8a7b0bc3152f22371291f2b8fbfd67b779715408
Tiago de Lima <tiago.blima@ufrpe.br>
2021-12-03 08:14:05 -0300

    precisa consertar o filtro

--- js/home.js
+++ js/home.js
@@ -32,6 +32,31 @@ getSkillsList = async function() {
             input.type = "checkbox"
             input.id = element.id
             input.value = element.id
+            
+            input.onclick = function(){
+
+              if(input.checked==true){
+                console.log(input.id);
+                getByIdRequest("opportunity/by/skill/", input.id).then(data => {
+
+                  data.forEach(element => {
+                    let divRow = createCard(element);
+                    document.getElementById("opportunityList").appendChild(divRow);
+                  });
+                })
+              }
+              else{
+                getByIdRequest("opportunity/by/skill/", input.id).then(data => {
+
+                  data.forEach(element => {
+                    let divRow = createCard(element);
+                    document.getElementById("opportunityList").removeChild(divRow);
+                  });
+                })
+              }
+
+            }
+
             let label = document.createElement("label")
             label.className = "form-check-label"
             label.innerHTML = element.name
@@ -56,18 +81,35 @@ async function getOpportunity(url)
     
 }
 
-function criaLinha(opportunity)
+
+const getByIdRequest = async(url, id) =>
+{   
+  //fetch(url).then(response => response.json).then(console.log);
+  
+  return await (await fetch(BASE_URL + url +`${id}/`)).json();
+  //showOpportunityDetails(await opportunity.json());
+  //const portoMember = await fetch(BASE_URL + "opportunity/by/id/2/");
+  //showPortoMemberDetails(portoMember);
+  //return await data.json();
+}
+
+function createCard(opportunity)
 {
+   //<span class="badge badge-pill badge-primary">Primary</span>
   divRow = document.createElement("div");
+  divRow.id = opportunity.id;
+ 
   divCol = document.createElement("div");
   h2 = document.createElement("h2");
   p0 = document.createElement("p");
   p1 = document.createElement("p");
   p2 = document.createElement("p");
   p3 = document.createElement("p");
+  p4 = document.createElement("p");
   bt = document.createElement("button");
+ 
 
-  divRow.className = "card mb-3 ml-3";
+  divRow.className = "card row mr-3 ml-3 mb-3";
   divCol.className = "col-md-8 card-body";
   h2.className = "card-title";
   h2.className = "card-text";
@@ -75,29 +117,36 @@ function criaLinha(opportunity)
   h2.className = "card-text";
   h2.className = "card-text";
   bt.className = "btn btn-primary";
+  bt.innerHTML =  "Detalhe"
+  
+ 
 
+
+  bt.onclick =  function (){
+      localStorage.setItem("id_Opoortunity", opportunity.id);
+      location.href = "./opportunityDetails.html";
+  }
  
   h2.innerHTML =  opportunity.description
   p1.innerHTML = 'Date Inicio: '+ opportunity.startDate
   p2.innerHTML = 'Data Fim: '+ opportunity.endDate
   p0.innerHTML = 'Valor R$: '+ opportunity.value
-  bt.innerHTML =  "Detalhe"
-  bt.onclick =  function (){
-      localStorage.setItem("id_Opoortunity", opportunity.id);
-      location.href = "./opportunityDetails.html";
-  }
-  
-  
-  divRow.appendChild(divCol);
+
+ 
+
   divCol.appendChild(h2);
   divCol.appendChild(p1);
   divCol.appendChild(p2);
   divCol.appendChild(p3);
+  divCol.appendChild(p4);
   divCol.appendChild(p0);
+ 
   divCol.appendChild(bt);
-
+  divRow.appendChild(divCol);
   return divRow;
+   
 
+ 
 
 }
 
@@ -151,6 +200,7 @@ const testIfPilarMember = async(id) =>
   //return await data.json();
 }
 
+
 function main()
 {
   
@@ -169,20 +219,14 @@ function main()
 
   console.log(data);
   let divRow = document.getElementById('div')
-  getOpportunity("opportunity/").then(opportunityList => {
-    opportunityList.forEach(element =>
-      {
-     
-       /*  let divRow = criaLinha(element)
-        divRow.appendChild(divCol)   */
-        let divCol = criaLinha(element)
-        divRow.appendChild(divCol) 
-        divRow.className = "row"; 
-    
-      
-      })
-  });
-  
+
+   
+  childNodes = document.getElementById('skillList').childNodes
+                
+ 
+
+
+ 
   
 }
 
